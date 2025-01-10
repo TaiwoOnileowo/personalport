cce6a1c3e8288b6d0438b263bd6f2f6491becf9c
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2025-01-11 00:00:31 +0100

    edit spelling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             {" "}
-            Hi, I&apos;m Taiwo, the FullStcak Developer for your next project
+            Hi, I&apos;m Taiwo, the FullStack Developer for your next project
           </p>
           <a href="#stack">
             <MagicButton
